c0b7e6cd006589cf38ce4669425712d606d4541b
pellumbMehmeti <p.mehmeti2@gmail.com>
2018-06-19 20:04:59 +0200

    Update ReservationRetrieve.java

--- app/src/main/java/com/projekti/grupi3/sportre/ReservationRetrieve.java
+++ app/src/main/java/com/projekti/grupi3/sportre/ReservationRetrieve.java
@@ -1,4 +1,52 @@
 package com.projekti.grupi3.sportre;
 
+
 public class ReservationRetrieve {
+
+    String FieldName,Date,Time,UserId;
+
+    public ReservationRetrieve()
+    {
+            //konstruktor pa parametra
+    }
+
+    public ReservationRetrieve(String fieldName,String date,String time,String usernameId)
+    {
+        FieldName=fieldName;
+        Date=date;
+        Time=time;
+        UserId=usernameId;
+
+    }
+
+    public String getFieldName() {
+        return FieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        FieldName = fieldName;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String usernameId) {
+        UserId = usernameId;
+    }
 }
