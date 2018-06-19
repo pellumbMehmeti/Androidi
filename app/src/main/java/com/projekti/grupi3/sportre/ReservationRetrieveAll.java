84398c1ca063a31005d2b4c58cb4660affa5451d
azejna <anite.zejna@gmail.com>
2018-06-19 20:00:22 +0200

    Test1

---
+++ app/src/main/java/com/projekti/grupi3/sportre/ReservationRetrieveAll.java
@@ -0,0 +1,13 @@
+package com.projekti.grupi3.sportre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationRetrieveAll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_retrieve_all);
+    }
+}
